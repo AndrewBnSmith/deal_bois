83e920e8431ccc0c9619fc7e4fc28ad1112e2d4f
AndrewBnSmith <89223936+AndrewBnSmith@users.noreply.github.com>
2022-04-25 13:40:32 -0500

    initial commit

---
+++ src/main/java/com/andrew/dealbois/DealBoisApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.dealbois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealBoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealBoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrew/dealbois/DealBoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.dealbois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealBoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
